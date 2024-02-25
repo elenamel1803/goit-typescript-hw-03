ecc410eb664ced85c9994f5e988d5f14a7995954
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-02-25 17:20:35 +0200

    add task solutions

--- src/2.ts
+++ src/2.ts
@@ -7,7 +7,6 @@
   властивість department, яка буде доступна лише всередині класу Employee.
   salary, яке буде доступне лише всередині класу Employee та його підкласів.
 
-
   Клас Manager повинен бути підклас класу Employee
 
   Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
@@ -15,16 +14,11 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -32,8 +26,9 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
 
 export {};
